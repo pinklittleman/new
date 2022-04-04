7c476a8138a23f13dcfc1597ba5c16739240072e
theo <80356772+pinklittleman@users.noreply.github.com>
2022-04-04 18:54:04 +0100

    yes

--- websockets/server.js
+++ websockets/server.js
@@ -19,31 +19,12 @@ io.on('connection', (socket) => {
   console.log('a user connected');
   users = users + 1
 
+  socket.on('message', (data) =>{
+    console.log(data)
+  })
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
     users = users - 1
   });
 });
-
-io.on('hello', function(users){
-  console.log(hello);
-});
-
-let x = 0
-//for testing, we're just going to send data to the client every second
-setInterval( function() {
-
-  /*
-    our message we want to send to the client: in this case it's just a random
-    number that we generate on the server
-  */
-
-  var users2 = ("users:  " + users);
-  io.emit('users', users2);
-  console.log (users2);
-
-  var msg = (x = x+1);
-  io.emit('message', msg);
-  console.log (msg);
-
-}, 1000);
