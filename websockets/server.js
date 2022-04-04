7059e00b682b64e4599d37e51f61baa1d1e81cdd
theo <80356772+pinklittleman@users.noreply.github.com>
2022-04-04 14:53:08 +0100

    yes

---
+++ websockets/server.js
@@ -0,0 +1,38 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+
+  //send the index.html file for all requests
+  res.sendFile(__dirname + '/index.html');
+
+});
+
+http.listen(5000, function(){
+
+  console.log('listening on *:3001');
+
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+//for testing, we're just going to send data to the client every second
+let x = 1
+setInterval( function() {
+
+  /*
+    our message we want to send to the client: in this case it's just a random
+    number that we generate on the server
+  */
+
+  var msg = (x = x + 1);
+  io.emit('message', msg);
+  console.log (msg);
+
+}, 1000);
