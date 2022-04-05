a23c1e6474d9ceafa053c775effa04a3e3afe9ff
theo <80356772+pinklittleman@users.noreply.github.com>
2022-04-05 10:27:50 +0100

    yes

--- websockets/server.js
+++ websockets/server.js
@@ -18,31 +18,15 @@ server.listen(5000, function(){
   console.log('listening on *:5000');
 
 });
-let users = 0
-let mad = false
-let count = 0
+let users = 0 
 io.on('connection', (socket) => {
   console.log('a user connected: ' + socket.id);
   users = users + 1
   socket.broadcast.emit('message', socket.id)
-
-  socket.on('num', (data) =>{
-    setInterval(() => {
-      console.log("x = :  " + data)
-    }, 2000);
-  })
-
   
   console.log(mad)
   socket.on('username', (data) =>{
     console.log(data)
-    if(mad === "true"){
-      if(count<1){
-        usernames.push(data)
-      }
-      count++
-    }
-    console.log("this is mad: "+mad)
     socket.broadcast.emit('message', data)
     console.log(usernames)
   })
