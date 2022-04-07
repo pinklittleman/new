70124fd18d4635b6cec1577c2ae49882d8b0daad
theo <80356772+pinklittleman@users.noreply.github.com>
2022-04-07 13:34:37 +0100

    yes

--- websockets/server.js
+++ websockets/server.js
@@ -1,3 +1,5 @@
+const { Socket } = require('socket.io');
+
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -7,3 +9,7 @@ app.get('/', function(req, res) {
 });
 
 server.listen(5000);
+
+io.on('connect', (socket) => {
+  console.log(socket.id)
+})
