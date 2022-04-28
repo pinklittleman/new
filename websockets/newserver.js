8d4c32ae3ed068a4aa806e6a23b133dabfb8abcc
theo <80356772+pinklittleman@users.noreply.github.com>
2022-04-28 14:04:50 +0100

    yes

---
+++ websockets/newserver.js
@@ -0,0 +1,18 @@
+const { countReset } = require('console');
+// does stuff
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.get('/', function(req, res){
+    //send the index.html file for all requests
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(5000, function(){
+    console.log('listening on :5000');
+});
+io.on('connection', (socket) => {
+    socket.emit('hello',"socket connected")
+    console.log('a user connected: ' + socket.id);
+})
