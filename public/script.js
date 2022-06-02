f42d87eb20870da1d7a39637b7186746f06d7b07
theo <80356772+pinklittleman@users.noreply.github.com>
2022-06-02 15:46:21 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,12 @@
 const socket = io('/')
-socket.emit('join-room', ROOM_ID, 10)
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+})
+
+myPeer.on('open', id =>{
+    socket.emit('join-room', ROOM_ID, id)
+})
 
 socket.on('userconnected', userId => {
     console.log('user connected: ' + userId )
