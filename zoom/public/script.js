5283556eb96118a2c1c17d2cc7961698252b4cae
theo <80356772+pinklittleman@users.noreply.github.com>
2022-06-02 19:52:05 +0100

    Update script.js

--- zoom/public/script.js
+++ zoom/public/script.js
@@ -7,7 +7,7 @@ const myPeer = new Peer(undefined, {
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
-navigator.mediaDevices.getUserMedia({
+mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
