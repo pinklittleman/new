efe495a464d6871517d372794f8324488a11235d
theo <80356772+pinklittleman@users.noreply.github.com>
2022-06-02 14:17:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-const express = require('express')
-const fs = require('fs')
+const express = require('express');
+const fs = require('fs');
 
-const app = express()
+const app = express();
+
 
 app.get('/video', (req, res) => {
-    const range = req.headers.range
-    const videoPath = './videos/help.mp4'
-    const videoSize = fs.statSync(videoPath).size
-    const chunkSize = 1 * 1e+6
-    const start = Number(range.replace(/\D/g, ''))
-    const end = Math.min(start + chunkSize, videoSize -1)
-    const contentLength = end - start + 1
-    const headers = {
-        'Content-Range': `bytes ${start}-${end}/${videoSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': contentLength,
-        'Content-Type': 'video/mp4'
-    }
-    res.WriteHead(206, headers)
-    const stream = fs.createReadStream(videoPath, {start, end})
-    stream.pipe(res)
-})
+    const range = req.headers.range;
+    const videoPath = './videos/help.mp4';
+    const videoSize = fs.statSync(videoPath).size;
 
-app.listen('3000')
+    const chunkSize = 1 * 1e+6;
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + chunkSize, videoSize -1);
+
+    const contentLength = end - start + 1;
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4"
+    }
+    res.writeHead(206, headers);
+
+    const stream = fs.createReadStream(videoPath, { start, end })
+    stream.pipe(res);
+});
+
+app.listen('3000');
