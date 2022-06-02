c6e4733ee83b0de913ab4363cca4d2448f530024
theo <80356772+pinklittleman@users.noreply.github.com>
2022-06-02 14:12:44 +0100

    add video

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 
 app.get('/video', (req, res) => {
-    console.log('hello world23')
+    const range = req.headers.range
+    const videoPath = './videos/help.mp4'
+    const videoSize = fs.statSync(videoPath).size
+    const chunkSize = 1 * 1e+6
+    const start = Number(range.replace(/\D/g, ''))
+    const end = Math.min(start + chunkSize, videoSize -1)
+    const contentLength = end - start + 1
+    const headers = {
+        'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': contentLength,
+        'Content-Type': 'video/mp4'
+    }
+    res.WriteHead(206, headers)
+    const stream = fs.createReadStream(videoPath, {start, end})
+    stream.pipe(res)
 })
 
 app.listen('3000')
