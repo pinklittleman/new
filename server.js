1a533e73be5bf53a93f27576150d8d87efcfe8e3
theo <80356772+pinklittleman@users.noreply.github.com>
2022-06-02 13:51:16 +0100

    Update server.js
    ger

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-const express = require
+const express = require('express')
+
+const app = express()
+
+app.length('/video', (req, res) => {
+    console.log('hello world23')
+})
+
+app.listen('3000')
