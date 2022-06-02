f8c861d14c1377a0db071358b24635aebb7e1b33
theo <80356772+pinklittleman@users.noreply.github.com>
2022-06-02 14:53:54 +0100

    s

--- server.js
+++ server.js
@@ -1,43 +1,6 @@
-const express = require('express');
-const fs = require('fs');
-var https = require("https");
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
-const app = express();
-
-
-app.get('/video', (req, res) => {
-    const range = req.headers.range;
-    const videoPath = './videos/help.mp4';
-    const videoSize = fs.statSync(videoPath).size;
-
-    const chunkSize = 1 * 1e+6;
-    const start = Number(range.replace(/\D/g, ''));
-    const end = Math.min(start + chunkSize, videoSize -1);
-
-    const contentLength = end - start + 1;
-
-    const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "video/mp4"
-    }
-    res.writeHead(206, headers);
-
-    const stream = fs.createReadStream(videoPath, { start, end })
-    stream.pipe(res);
-});
-
-https
-  .createServer(
-    {
-      key: fs.readFileSync("server.key"),
-      cert: fs.readFileSync("server.cert"),
-    },
-    app
-  )
-  .listen(3000, function () {
-    console.log(
-      "Example app listening on port 3000! Go to https://localhost:3000/"
-    );
-});
+server.listen('5000')
