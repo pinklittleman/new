6507739ce42e15dd6b1eafebcee32dd8ca445369
theo <80356772+pinklittleman@users.noreply.github.com>
2022-06-03 09:15:43 +0100

    deleting unused assetes

--- websockets/server.js
+++ websockets/server.js
@@ -1,111 +1,54 @@
 const { countReset } = require('console');
-// does stuff
+// setup
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
-let currentSketch = []
-
-let usercount = []
-
-let colours = []
-
-var temp1
-var temp2
-var temp3
-
-var newval
-
+//save the cords to an array
+let cords = []
+let user = []
+let count = user.length
+//get the index file for connection
 app.get('/', function(req, res){
-
-  //send the index.html file for all requests
-  res.sendFile(__dirname + '/index.html');
-
+    //send the index.html file for all requests
+    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/Synthwave4.mp3');
+    
 });
-
+//make the server listen on port 5000 
 server.listen(5000, function(){
-
-  console.log('listening on :5000');
-
+    console.log('listening on :5000');
 });
-var usr
-var user = {}
+//on connection to the socket do stuff
 io.on('connection', (socket) => {
-  console.log('a user connected: ' + socket.id);
-  user = socket.id
-  socket.broadcast.emit('message', socket.id)
-  socket.emit('connection', 'connection established')
-  socket.emit('sketch', currentSketch)
-
-  socket.on('rgb', (number1, number2, number3) => {
-    temp1 = number1
-    temp2 = number2
-    temp3 = number3
+    // log the user that has connected and their socketID
+    console.log('a user connected: ' + socket.id);
+    socket.emit('ID', socket.id)
+    user.push(socket.id)
+    console.log(user)
+    // when the socket recives files from the client about mouse position call the logging function
+    
+    socket.on('cordinates', logging)
     setInterval(() => {
-      setTimeout(() => {
-        // colours.push(number1,number2,number3)
-      }, 1000);
+        count =  user.length
+        socket.emit('userarray', count)
+        // console.log(count)
+        socket.emit('userarrayimp', user)
     }, 2000);
-    
-    setInterval(() => {
-      socket.broadcast.emit('newrgb',temp1,temp2,temp3)
-      console.log(colours)
-    }, 1000);
-  })
 
-  socket.on('username', logname)
-  
-  socket.on('mouse', mouseMsg)
+    // this function adds the cordinates to the array cords
 
-  socket.on('refresh', aler)
+    function logging(data){
+        cords.push(data)
+        socket.broadcast.emit('newcords',data)
+        //console.log(cords) uncomment if you want laggggggg
+    }
 
-  socket.on('size', size)
+    socket.on('disconnect', () => {
+        console.log('leaving: '+socket.id)
+        var pos = user.indexOf(socket.id)
+        user.splice(pos,pos+1)
+        console.log(user)
+    })
 
-  socket.on('clear', arrayclear)
+})
 
-  function size(data){
-    newval = data
-    socket.broadcast.emit('newsize', newval)
-    
-  }
-
-  function aler(data){
-    socket.broadcast.emit('reload', data)
-  }
-
-  function logname(data){
-      usr = {
-        username: data,
-        id: socket.id,
-      }
-    
-    console.log(usr)
-    usercount.push(usr.username)
-    setInterval(() => {
-      socket.broadcast.emit('users', usr)
-      setTimeout(() => {
-        console.log(usercount)
-      }, 3000);
-    }, 2000);
-  }
-
-  function arrayclear(){
-    currentSketch.length = 0;
-    currentSketch;
-  }
-
-  function mouseMsg(data){
-    socket.broadcast.emit('incords', data)
-    currentSketch.push(data)
-  }
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected ' + usr.username+" "+user.id);
-    var search_term = usr.username;
-    for (var i=usercount.length-1; i>=0; i--) {
-      if (usercount[i] === search_term) {
-        usercount.splice(i, 1);
-        break;
-    }}
-  });
-});
