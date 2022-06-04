6fbb575dc2a1fda49def38fdbbce0545b1998680
theo <80356772+pinklittleman@users.noreply.github.com>
2022-06-04 16:05:30 +0100

    Update server.js

--- 2d-multiplayer/server.js
+++ 2d-multiplayer/server.js
@@ -1,3 +1,5 @@
+const { countReset } = require('console');
+// setup
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -8,16 +10,36 @@ let count = user.length
 //get the index file for connection
 app.get('/', function(req, res){
     //send the index.html file for all requests
-    res.sendFile(__dirname + '/index.html');    
+    res.sendFile(__dirname + '/index.html'); 
 });
 //make the server listen on port 5000 
 server.listen(5000, function(){
     console.log('listening on :5000');
 });
-
+//on connection to the socket do stuff
 io.on('connection', (socket) => {
     // log the user that has connected and their socketID
     console.log('a user connected: ' + socket.id);
+    socket.emit('ID', socket.id)
+    user.push(socket.id)
+    console.log(user)
+    // when the socket recives files from the client about mouse position call the logging function
+    
+    socket.on('cordinates', logging)
+    setInterval(() => {
+        count =  user.length
+        socket.emit('userarray', count)
+        // console.log(count)
+        socket.emit('userarrayimp', user)
+    }, 2000);
+
+    // this function adds the cordinates to the array cords
+
+    function logging(data){
+        cords.push(data)
+        socket.broadcast.emit('newcords',data)
+        //console.log(cords) uncomment if you want laggggggg
+    }
 
     socket.on('disconnect', () => {
         console.log('leaving: '+socket.id)
@@ -26,4 +48,5 @@ io.on('connection', (socket) => {
         console.log(user)
     })
 
-});
+})
+
