1189d6ed11653bfa5043c47739bf6b5b2b7a1af1
theo <80356772+pinklittleman@users.noreply.github.com>
2022-06-05 15:58:30 +0100

    Update server.js

--- 2d-multiplayer/server.js
+++ 2d-multiplayer/server.js
@@ -11,6 +11,7 @@ let count = user.length
 app.get('/', function(req, res){
     //send the index.html file for all requests
     res.sendFile(__dirname + '/index.html'); 
+    res.sendFile(__dirname + '/test.png')
 });
 //make the server listen on port 5000 
 server.listen(5000, function(){
