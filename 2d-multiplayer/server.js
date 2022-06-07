ed98c2315f6efce273ca0b1134015c62e7de70c2
theo <80356772+pinklittleman@users.noreply.github.com>
2022-06-07 20:23:36 +0100

    test with socket.io

--- 2d-multiplayer/server.js
+++ 2d-multiplayer/server.js
@@ -12,18 +12,37 @@ var https = require('https');
 var httpsServer = https.createServer(credentials);
 httpsServer.listen(5000);
 
-var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({
-    server: httpsServer
-});
+var io = require('socket.io')(httpsServer);
 
-var stuff
+io.on('connection', (socket) => {
+    // log the user that has connected and their socketID
+    console.log('a user connected: ' + socket.id);
+    socket.emit('ID', socket.id)
+    user.push(socket.id)
+    console.log(user)
+    // when the socket recives files from the client about mouse position call the logging function
+    
+    socket.on('cordinates', logging)
+    setInterval(() => {
+        count =  user.length
+        socket.emit('userarray', count)
+        // console.log(count)
+        socket.emit('userarrayimp', user)
+    }, 2000);
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
-        ws.send('reply from server : ' + stuff)
-    });
+    // this function adds the cordinates to the array cords
 
-    ws.send('something');
-});
+    function logging(data){
+        cords.push(data)
+        socket.broadcast.emit('newcords',data)
+        //console.log(cords) uncomment if you want laggggggg
+    }
+
+    socket.on('disconnect', () => {
+        console.log('leaving: '+socket.id)
+        var pos = user.indexOf(socket.id)
+        user.splice(pos,pos+1)
+        console.log(user)
+    })
+
+})
