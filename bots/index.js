f28a13f4565ffdec2da888fbe4c83ca524d40402
theo <80356772+pinklittleman@users.noreply.github.com>
2022-06-28 15:25:26 +0100

    Update index.js

--- bots/index.js
+++ bots/index.js
@@ -1,9 +1,13 @@
 const discord = require('discord.js')
+const { createAudioPlayer } = require('@discordjs/voice');
 require('dotenv').config();
 const Client = new discord.Client({
   intents: [ discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_MESSAGES, discord.Intents.FLAGS.GUILD_VOICE_STATES ]
 })
 const /*you can have any prefix you want here*/ prefix = "?"
+
+const player = createAudioPlayer();
+const resource = createAudioResource('./bingus.mp3');
 
 Client.on("ready", () => {
 	console.log('ready to do heroin')
@@ -17,9 +21,8 @@ Client.on('messageCreate', message => {
             channelId: message.member.voice.channel.id,
             guildId: message.guild.id,
             adapterCreator: message.guild.voiceAdapterCreator
-        }).then(connection => {
-            connection.play('./bingus.mp3')
         })
+        player.play(resource);
 
     }
 })
