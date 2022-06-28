304e2bef147cd6d979817c879c939d00ddf79a62
theo <80356772+pinklittleman@users.noreply.github.com>
2022-06-28 14:55:35 +0100

    Update index.js

--- bots/index.js
+++ bots/index.js
@@ -39,4 +39,4 @@ Client.on('messageCreate', message => {
 
 //EXTREMELY IMPORTANT: GET YOUR TOKEN FROM THE DISCORD DEVELOPER PORTAL
 //NEVER EVER EVER EVER TELL/GIVE ANYONE YOUR TOKEN!
-Client.login('');
+client.login(process.env.DISCORD_TOKEN);
