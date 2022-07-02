014a40c7c91f8fe22a802ee2aa828e5e79d725c3
theo <80356772+pinklittleman@users.noreply.github.com>
2022-07-02 23:33:17 +0100

    change of server items

--- 2d-multiplayer/server.js
+++ 2d-multiplayer/server.js
@@ -9,7 +9,6 @@ var credentials = { key: privateKey, cert: certificate};
 var https = require('https')
 
 let users = []
-var roomnum = 1;
 
 //pass in your credentials to create an https server
 var httpsServer = https.createServer(credentials, app);
@@ -24,38 +23,18 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', (socket) => {
-    if(users.length % 2 === 0 && users.length !== 0){
-        console.log('even')
-        roomnum = roomnum + 1
-        console.log(roomnum)
-    }
-    else{
-        console.log('no new room')
-    }
-    socket.join("room-"+roomnum);
     // log the user that has connected and their socketID
-    io.sockets.in("room-"+roomnum).emit('connectToRoom', "You are in room no. "+roomnum);
     console.log('a user connected: ' + socket.id);
     users.push(socket.id)
     console.log(users)
 
     socket.on('cords', ball)
-    
-    function ball(data){
-        socket.to("room-"+roomnum).emit('newball', data)
-    }
 
     socket.on('disconnect', () => {
         console.log('leaving: '+socket.id)
         var pos = users.indexOf(socket.id)
         users.splice(pos,pos+1)
         console.log(users)
-        console.log(roomnum)
     })
-
-    
-    setInterval(() => {
-        socket.emit('list', users)
-    }, 2000);
 
 })
