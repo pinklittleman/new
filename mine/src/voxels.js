349eb56b406eb1c89b7809b9e7c3b5a4a7cd646f
theo <80356772+pinklittleman@users.noreply.github.com>
2022-07-12 14:17:37 +0100

    Update voxels.js

--- mine/src/voxels.js
+++ mine/src/voxels.js
@@ -9,7 +9,7 @@ import {voxels_tool} from './voxels_tool.js';
 
 export const voxels = (function() {
 
-  const _VOXEL_HEIGHT = 256;
+  const _VOXEL_HEIGHT = 512;
   const _OCEAN_LEVEL = Math.floor(_VOXEL_HEIGHT * 0.12);
   const _BEACH_LEVEL = _OCEAN_LEVEL + 2;
   const _SNOW_LEVEL = Math.floor(_VOXEL_HEIGHT * 0.8);
