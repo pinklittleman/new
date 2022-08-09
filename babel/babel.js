45ac7eefad70c28bee122298fbc903b5213fbc21
theo <80356772+pinklittleman@users.noreply.github.com>
2022-08-09 15:03:46 +0100

    Update babel.js

--- babel/babel.js
+++ babel/babel.js
@@ -1,1 +1,11 @@
 let canvas = document.querySelector('canvas')
+let ctx = canvas.getContext('2d')
+
+canvas.width = 800
+canvas.height = 400
+
+ctx.fillStyle = '#F0DB4F';
+ctx.strokeStyle = 'red';
+
+ctx.fillRect(50, 50, 150, 100);
+ctx.strokeRect(50, 50, 150, 100);
