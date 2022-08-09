94d29aa6ec97b05cf83e13ee9af006315b8475f4
theo <80356772+pinklittleman@users.noreply.github.com>
2022-08-09 15:31:55 +0100

    Update babel.js

--- babel/babel.js
+++ babel/babel.js
@@ -9,10 +9,13 @@ let colours = ['red', 'green', 'blue']
 ctx.fillStyle = 'red';
 
 function poggers(){
-    for (let i = 0; i <= 800; i++) {
-        var randColor = colours[Math.floor(Math.random() * colours.length)];
-        ctx.fillStyle = randColor
-        ctx.fillRect(i, 50, 1, 1);
-        
-    }   
+    for (let y = 0; y <= 400; y++){
+        for (let x = 0; x <= 800; x++) {
+            var randColor = colours[Math.floor(Math.random() * colours.length)];
+            ctx.fillStyle = randColor
+            ctx.fillRect(i, 50, 1, 1);
+            
+        }
+    }
+       
 }
