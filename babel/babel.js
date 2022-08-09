5f735a5a9a4b531b988eac034a782273b0edd88c
theo <80356772+pinklittleman@users.noreply.github.com>
2022-08-09 15:30:05 +0100

    Update babel.js

--- babel/babel.js
+++ babel/babel.js
@@ -4,11 +4,15 @@ let ctx = canvas.getContext('2d')
 canvas.width = 800
 canvas.height = 400
 
-ctx.fillStyle = '#F0DB4F';
-ctx.strokeStyle = 'red';
+let colours = ['red', 'green', 'blue']
+
+ctx.fillStyle = 'red';
 
 function poggers(){
     for (let i = 0; i <= 800; i++) {
+        var randColor = colours[Math.floor(Math.random() * colours.length)];
+        ctx.fillStyle = randColor
         ctx.fillRect(i, 50, 1, 1);
-    }
+        
+    }   
 }
