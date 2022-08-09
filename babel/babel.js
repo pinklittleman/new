5243be082823224a1ec11474699477ef5a1f4a4b
theo <80356772+pinklittleman@users.noreply.github.com>
2022-08-09 17:40:52 +0100

    Update babel.js

--- babel/babel.js
+++ babel/babel.js
@@ -5,7 +5,7 @@ let ctx = canvas.getContext('2d')
 canvas.width = 900
 canvas.height = 900
 
-let colours = ['red', 'green', 'blue','black','white','grey','fc53da']
+let colours = ['red', 'green', 'blue','black','white','grey','yellow','orange']
 let x = 0
 let y = 0
 let size1 = prompt("please enter how big you want the pixels to be 1-250 !!! MUST BE DIVISABLE BY 5")
