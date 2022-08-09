87c8a654d26ae09dc68e6a20389428580bbaaed8
theo <80356772+pinklittleman@users.noreply.github.com>
2022-08-09 19:10:15 +0100

    Update babel.js

--- babel/babel.js
+++ babel/babel.js
@@ -5,23 +5,29 @@ let ctx = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-let colours = ['black','white']
+let colours = ['red', 'green', 'blue','black','white','grey','yellow','orange','pink','#e31010','#5be310','#9810e3','#1089e3']
 let x = 0
 let y = 0
 
-for (let i = 1; i <= canvas.width; i++) {
-    var randColor = colours[Math.floor(Math.random() * colours.length)];
-    ctx.fillStyle = randColor
-    ctx.fillRect(x, y, 1, 1);
-    x++
-    if(x >= canvas.width){
-        i = 0
-        x = 0
-        y++
+setInterval(() => {
+    art()
+}, 5000);
+
+function art(){
+    for (let i = 1; i <= canvas.width; i++) {
+        var randColor = colours[Math.floor(Math.random() * colours.length)];
+        ctx.fillStyle = randColor
+        ctx.fillRect(x, y, 1, 1);
+        x++
+        if(x >= canvas.width){
+            i = 0
+            x = 0
+            y++
+        }
+        if(y >= canvas.height){
+            break
+        }      
     }
-    if(y >= canvas.height){
-        break
-    }      
 }
 
 
