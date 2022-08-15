6001d9e2d5d17af9e977959b8c0617c6ca56e519
theo <80356772+pinklittleman@users.noreply.github.com>
2022-08-15 22:38:37 +0100

    Update babel.js

--- babel/babel.js
+++ babel/babel.js
@@ -3,14 +3,19 @@ document.getElementById('hide_id').style.cursor = 'none';
 
 const socket = io.connect('wss://pinky.uk.to:4000');
 
+let ball
+
 function setup() {
     createCanvas(innerWidth,innerHeight);
 }
   
 function draw() {
     background(66);
-    ellipse(50,50,80,80);
+    ball = ellipse(50,50,80,80);
+    
 }
 
-
+function mouseMoved(){
+    ball.translate(mouseX,mouseY)
+}
 
