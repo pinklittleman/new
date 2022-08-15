32e1ea55e839c7f44cc12e0d998d032a1d3ec517
theo <80356772+pinklittleman@users.noreply.github.com>
2022-08-15 21:19:44 +0100

    Update babel.js

--- babel/babel.js
+++ babel/babel.js
@@ -1,7 +1,7 @@
 
 document.getElementById('hide_id').style.cursor = 'none';
 
-socket = io.connect('http://pinky.uk.to:4000')
+socket = io.connect('https://pinky.uk.to:4000')
 
 let canvas = document.querySelector('canvas')
 let ctx = canvas.getContext('2d')
