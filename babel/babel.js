f4d7a67672687b4d74be23a02cb8a8a893aae2d5
theo <80356772+pinklittleman@users.noreply.github.com>
2022-08-15 22:33:45 +0100

    Update babel.js

--- babel/babel.js
+++ babel/babel.js
@@ -4,7 +4,7 @@ document.getElementById('hide_id').style.cursor = 'none';
 const socket = io.connect('wss://pinky.uk.to:4000');
 
 function setup() {
-    createCanvas(innerHeight, innerWidth);
+    createCanvas(innerWidth,innerHeight);
 }
   
 function draw() {
