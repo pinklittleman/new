69b8e9aa15cb872234cbfa27eade826ba98f85ce
theo <80356772+pinklittleman@users.noreply.github.com>
2022-08-15 20:05:52 +0100

    draw function added

--- babel/babel.js
+++ babel/babel.js
@@ -10,12 +10,15 @@ let y = 0
 let players = []
 
 class Player{
-    constructor(x,y,size,color,velocity){
+    constructor(x,y,size,color){
         this.x = x
         this.y = y
         this.size = size
         this.color =  color
-        this.velocity = velocity
+    }
+    draw(){
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x,this.y,this.size,this.size)
     }
     update(){
         this.draw()
