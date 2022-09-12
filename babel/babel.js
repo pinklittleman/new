525ec2ba91227bf251c37cb0058b62ae178e4d25
theo <80356772+pinklittleman@users.noreply.github.com>
2022-09-12 13:16:55 +0100

    restore babel

--- babel/babel.js
+++ babel/babel.js
@@ -1,19 +1,34 @@
+let canvas = document.querySelector('canvas')
+let ctx = canvas.getContext('2d')
 
-const socket = io.connect('wss://pinky.uk.to:4000');
+canvas.width = innerWidth
+canvas.height = innerHeight
 
-let ball
+let colours = ['red', 'green', 'blue','black','white','grey','yellow','orange','pink','#e31010','#5be310','#9810e3','#1089e3']
+let x = 0
+let y = 0
 
-function setup() {
-    createCanvas(innerWidth,innerHeight);
+setInterval(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    art()
+}, 2000);
+
+function art(){
+    for (let i = 1; i <= canvas.width; i++) {
+        var randColor = colours[Math.floor(Math.random() * colours.length)];
+        ctx.fillStyle = randColor
+        ctx.fillRect(x, y, 1, 1);
+        x++
+        if(x >= canvas.width){
+            i = 0
+            x = 0
+            y++
+            console.log('new line')
+        }
+        if(y >= canvas.height){
+            y = 0
+            console.log('finished')
+            break
+        }      
+    }
 }
-  
-function draw() {
-    let x = mouseX
-    let y = mouseY
-    background(66);
-    rect(x,y,80,80)
-    fill(50)
-    
-    
-}
-
