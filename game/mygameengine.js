e9987d660697bb5ae47cefe6494864d12e79d388
theo <80356772+pinklittleman@users.noreply.github.com>
2022-09-20 00:19:32 +0100

    Update mygameengine.js

--- game/mygameengine.js
+++ game/mygameengine.js
@@ -1,12 +1,11 @@
 let canvas = document.querySelector('canvas')
 let ctx = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
-
 function animate(){
     requestAnimationFrame(animate)
     console.log('loop')
+    canvas.width = innerWidth
+    canvas.height = innerHeight
 }
 
 animate()
