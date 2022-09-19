7556f1225a54f41beedbdd643fb4ed794b98bb51
theo <80356772+pinklittleman@users.noreply.github.com>
2022-09-19 23:48:45 +0100

    Update yourmother.js

--- three/yourmother.js
+++ three/yourmother.js
@@ -5,7 +5,7 @@ const scene = new THREE.Scene();
 			renderer.setSize( window.innerWidth, window.innerHeight );
 			document.body.appendChild( renderer.domElement );
 
-			const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			const geometry = new THREE.BoxGeometry( 2, 1, 1 );
 			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 			const cube = new THREE.Mesh( geometry, material );
 			scene.add( cube );
