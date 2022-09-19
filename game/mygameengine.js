f65e5a05125d53b66c1dda5b92e1f37294be1727
theo <80356772+pinklittleman@users.noreply.github.com>
2022-09-20 00:09:01 +0100

    game

---
+++ game/mygameengine.js
@@ -0,0 +1,12 @@
+let canvas = document.getElementById('canvas')
+let ctx = canvas.getContex('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+function animate(){
+    requestAnimationFrame(animate)
+    
+}
+
+animate()
