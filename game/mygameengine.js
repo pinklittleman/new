dcd631d23b8a13f8d006d0789f232cf3af672ae8
theo <80356772+pinklittleman@users.noreply.github.com>
2022-09-20 17:29:12 +0100

    Update mygameengine.js

--- game/mygameengine.js
+++ game/mygameengine.js
@@ -1,5 +1,11 @@
+const { arch } = require("os")
+
 let canvas = document.querySelector('canvas')
 let ctx = canvas.getContext('2d')
+
+ctx.beginPath()
+ctx.arc(100,100,20,0,2*Math.PI)
+ctx.fillStyle('#424242')
 
 function animate(){
     requestAnimationFrame(animate)
