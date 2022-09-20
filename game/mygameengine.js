5efa2e145c79abb2dc3a78c3bc637cc9e94e5a7f
theo <80356772+pinklittleman@users.noreply.github.com>
2022-09-20 22:25:38 +0100

    Update mygameengine.js

--- game/mygameengine.js
+++ game/mygameengine.js
@@ -27,14 +27,6 @@ class Ball{
     }
 
     drawVel(){
-        var red, green, blue;
-
-        for (red = 0; red <= 255; red++) {
-            for (green = 0; green <= 255; green++) {
-                for (blue = 0; blue <= 255; blue++) {
-                }
-            }
-        }
         ctx.fillStyle = rgb(red, green, blue);
         ctx.fillText('x: '+this.x+' y: '+this.y, this.x - 5, this.y);
     }
