646a9ac471ac2d92cc9c2f66925f4c93f5b68795
theo <80356772+pinklittleman@users.noreply.github.com>
2022-09-26 20:51:46 +0100

    Update mygameengine.js

--- game/mygameengine.js
+++ game/mygameengine.js
@@ -179,6 +179,15 @@ function gameloop(){
     if(intersects(square1.x,square1.y,square1.w,square1.h,Block1.x,Block1.y,Block1.w,Block1.h) === true){
         square1.vel_x = 8
     }
+    if(intersects(square1.x,square1.y,square1.w,square1.h,Block2.x,Block2.y,Block2.w,Block2.h) === true){
+        square1.vel_x = 8
+    }
+    if(intersects(square1.x,square1.y,square1.w,square1.h,Block3.x,Block3.y,Block3.w,Block3.h) === true){
+        square1.vel_x = 8
+    }
+    if(intersects(square1.x,square1.y,square1.w,square1.h,Block4.x,Block4.y,Block4.w,Block4.h) === true){
+        square1.vel_x = 8
+    }
 
     requestAnimationFrame(gameloop);
 }
