7d75830056aa7d328ab2127f303f14bbd6f1a6dc
theo <80356772+pinklittleman@users.noreply.github.com>
2022-10-08 19:43:10 +0100

    weird blue bottom wall

--- game/mygameengine.js
+++ game/mygameengine.js
@@ -315,7 +315,7 @@ let square1 = new Ball(200,200,30,30,socket.id);
 let Block1 = new Block(0,0,10,1080)
 let Block2 = new Block(0,0,1920,10)
 let Block3 = new Block(canvas.width-5,0,10,1080)
-let Block4 = new Block(0,canvas.height-5,1920,10)
+// let Block4 = new Block(0,canvas.height-5,1920,10)
 let car1 = new Car(300,300,50,70)
 square1.player = true;
 
