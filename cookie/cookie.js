07e1dbb5e871158698a13a13fd65b0267ecb06d8
theo <80356772+pinklittleman@users.noreply.github.com>
2022-10-10 20:28:33 +0100

    Update cookie.js

--- cookie/cookie.js
+++ cookie/cookie.js
@@ -1,13 +1,17 @@
-let deg = 90
-let cookie2 = document.getElementById('cookie')
+setTimeout(() => {
 
-canh = innerHeight / 2
+    let deg = 90
+    let cookie2 = document.getElementById('cookie')
 
-cookie2.style.top = canh
+    canh = innerHeight / 2
 
-function cookie() {
-    cookie2.style.transform = "rotate("+(deg)+"deg)"
-    deg += Math.floor((Math.random()* 20) + -10)
-}
+    cookie2.style.top = canh
+
+    function cookie() {
+        cookie2.style.transform = "rotate("+(deg)+"deg)"
+        deg += Math.floor((Math.random()* 20) + -10)
+    }
 
 document.addEventListener("click", cookie)
+    
+}, 1000);
