2e3f5d32505456138b7e7264d4cd56bffebc99ae
theo <80356772+pinklittleman@users.noreply.github.com>
2022-10-10 20:22:29 +0100

    Update cookie.js

--- cookie/cookie.js
+++ cookie/cookie.js
@@ -3,7 +3,7 @@ deg = 90
 function cookie() {
     let cookie = document.getElementById('cookie')
     cookie.style.transform = "rotate("+(deg)+"deg)"
-    deg += Math.floor((Math.random()* 50) + -10)
+    deg += Math.floor((Math.random()* 20) + -10)
 }
 
 document.addEventListener("click", cookie)
