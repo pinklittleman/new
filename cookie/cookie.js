006ff51eb1af7026ffe7d25efbc3e034dc01fadf
theo <80356772+pinklittleman@users.noreply.github.com>
2022-10-11 13:10:22 +0100

    le load

--- cookie/cookie.js
+++ cookie/cookie.js
@@ -1,19 +1,23 @@
 let cookies = 0
 let multiplier = 1, adding = 1
 let clicked = false, multi
-let deg = 90
+let deg = 90, cookie2, loading, cookienum
 
 const sleep = (time) => {
     return new Promise((resolve) => setTimeout(resolve, time))
 }
 
-async function cookie() {
-    await sleep(5000)
-    let cookie2 = document.getElementById('cookie')
-    let loading = document.getElementById('loading')
-    let cookienum = document.getElementById('cookie2')
+
+async function loading(){
+    await sleep(50)
+    cookie2 = document.getElementById('cookie')
+    loading = document.getElementById('loading')
+    cookienum = document.getElementById('cookie2')
     canh = innerHeight / 2 - 100
     cookie2.style.top = canh+"px"
+}
+
+async function cookie(){
     loading.style.visibility = 'hidden'
     cookie2.style.transform = "rotate("+(deg)+"deg)"
     deg += Math.floor((Math.random()* 20) + -10)
