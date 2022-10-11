c6adf7e0bac27dc7420528a77ae70fe95bddb766
theo <80356772+pinklittleman@users.noreply.github.com>
2022-10-11 12:57:11 +0100

    Update cookie.js

--- cookie/cookie.js
+++ cookie/cookie.js
@@ -1,38 +1,31 @@
 let cookies = 0
 let multiplier = 1, adding = 1
 let clicked = false, multi
-
-setTimeout(() => {
-    
-    let deg = 90
-    let cookie2 = document.getElementById('cookie')
-    let loading = document.getElementById('loading')
-    let cookienum = document.getElementById('cookie2')
-
-    canh = innerHeight / 2 - 100
-
-    cookie2.style.top = canh+"px"
-
-    function cookie() {
-        cookie2.style.transform = "rotate("+(deg)+"deg)"
-        deg += Math.floor((Math.random()* 20) + -10)
-        Math.floor(cookies += adding * multiplier)
-        cookienum.innerHTML = cookies
-        if(cookies >= 100){
-            document.getElementById('mult').style.visibility = 'visible'
-            if (clicked === true) {
-                cookies -= 100
-                multiplier += 2
-                clicked = false
-            }
-        }
-        else{
-            document.getElementById('mult').style.visibility = 'hidden'
-        }
-    }
+let deg = 90
+let cookie2 = document.getElementById('cookie')
+let loading = document.getElementById('loading')
+let cookienum = document.getElementById('cookie2')
+canh = innerHeight / 2 - 100
+cookie2.style.top = canh+"px"
 loading.style.visibility = 'hidden'
 
-}, 1000);
+function cookie() {
+    cookie2.style.transform = "rotate("+(deg)+"deg)"
+    deg += Math.floor((Math.random()* 20) + -10)
+    Math.floor(cookies += adding * multiplier)
+    cookienum.innerHTML = cookies
+    if(cookies >= 100){
+        document.getElementById('mult').style.visibility = 'visible'
+        if (clicked === true) {
+            cookies -= 100
+            multiplier += 2
+            clicked = false
+        }
+    }
+    else{
+        document.getElementById('mult').style.visibility = 'hidden'
+    }
+}
 
 function click2(){
     clicked = true
